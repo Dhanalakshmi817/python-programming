dd87a2f1d89deb6ac7f6ea1c55b0df0e9924df20
Dhanalakshmi817 <dhanadoddi986@gmail.com>
2026-09-20 22:44:18 +0530

    prime

---
+++ python basics/prime.py
@@ -0,0 +1,12 @@
+n=int(input("enter a number:"))
+count=0
+for i in range(1,n+1):
+   if(n<=1):
+     print("not prime")
+   if(n%i==0):
+     count+=1
+   if(count==2):
+     print("prime")
+   else:
+     print("not prime")
+                
