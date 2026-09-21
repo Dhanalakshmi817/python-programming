0b6712645de24c4b88e1352c29d9861fa8dfe456
Dhanalakshmi817 <dhanadoddi986@gmail.com>
2026-09-21 22:24:14 +0530

    add book page

---
+++ python basics/Library_Management_System/static/css/Library_Management_System/templates/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/books")
+def books():
+    return render_template("books.html")
+
+@app.route("/add")
+def add():
+    return render_template("add.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
