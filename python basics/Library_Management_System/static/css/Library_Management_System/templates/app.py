cee0eaf9ab207d2ecc06e332146c25d8b7d0637f
Dhanalakshmi817 <dhanadoddi986@gmail.com>
2026-09-21 23:26:20 +0530

    Added SQLite database and book management

--- python basics/Library_Management_System/static/css/Library_Management_System/templates/app.py
+++ python basics/Library_Management_System/static/css/Library_Management_System/templates/app.py
@@ -1,7 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import sqlite3
 
 app = Flask(__name__)
 
+def create_db():
+    con = sqlite3.connect("library.db")
+    cur = con.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS books (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            author TEXT,
+            status TEXT
+        )
+    """)
+
+    con.commit()
+    con.close()
+
+create_db()
 
 @app.route("/")
 def home():
@@ -9,10 +27,35 @@ def home():
 
 @app.route("/books")
 def books():
-    return render_template("books.html")
+    con = sqlite3.connect("library.db")
+    con.row_factory = sqlite3.Row
 
-@app.route("/add")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM books")
+
+    data = cur.fetchall()
+    con.close()
+
+    return render_template("books.html", books=data)
+
+@app.route("/add", methods=["GET", "POST"])
 def add():
+    if request.method == "POST":
+        name = request.form["name"]
+        author = request.form["author"]
+        status = request.form["status"]
+
+        con = sqlite3.connect("library.db")
+        cur = con.cursor()
+
+        cur.execute(
+            "INSERT INTO books (name, author, status) VALUES (?, ?, ?)",
+            (name, author, status)
+        )
+
+        con.commit()
+        con.close()
+
     return render_template("add.html")
 
 if __name__ == "__main__":
